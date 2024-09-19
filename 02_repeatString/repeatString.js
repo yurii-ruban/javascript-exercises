2b55de87029812c6d890c1b661e1f1d47d87df3d
Yurii Ruban <yurii.ruban97@gmail.com>
2024-09-19 15:57:31 +0100

    Solve 0-3 excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if (count < 0 ) return "ERROR";
+    if (count == 0) return "";
 
+    let result = "";
+    for (let i = 0; i < count; ++i) {
+        result += str;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arrStr = str.split("");
 
+    for (let i = 0; i < arrStr.length / 2; ++i) {
+        let temp = arrStr[i];
+        arrStr[i] = arrStr[arrStr.length - 1 - i];
+        arrStr[arrStr.length - 1 - i] = temp;
+    }
+
+    return arrStr.join("");
 };
 
 // Do not edit below this line
