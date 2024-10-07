3fb48c5a14028e5003591eb77f52fc4f76224a4b
Yurii Ruban <yurii.ruban97@gmail.com>
2024-10-07 19:36:23 +0100

    Finish task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
 
+    let arr = [0, 1, 1];
+    for (let i = 3; i <= Number(n); ++i) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
