89d34355fdda6994253803c5798cb6585ce79537
Yurii Ruban <yurii.ruban97@gmail.com>
2024-10-07 17:09:22 +0100

    Solve Task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, val) => total + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, val) => total * val, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
