6124d843ca28f2992ec25f31944a29ea83e2760d
Yurii Ruban <yurii.ruban97@gmail.com>
2024-10-07 20:09:36 +0100

    Finish task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+function getAge(person) {
+    person.yearOfDeath ??= currentYear;
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+    if (people.length === 0) return {};
+
+    return people.reduce((oldest, person) => {
+        const personAge = getAge(person);
+        const oldestAge = getAge(oldest)
+
+        if (personAge > oldestAge) {
+            oldest = Object.assign({}, person);
+        }
+
+        return oldest;
+    }, Object.assign({}, people[0]));
 };
 
 // Do not edit below this line
