2d2f3b513e73001c37e3c8113460ce40c4671245
Yurii Ruban <yurii.ruban97@gmail.com>
2024-10-07 17:27:55 +0100

    Finish task 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
+const palindromes = function (str) {
+    const cleaned = str.toLowerCase()
+                        .split("")
+                        .filter((val) => ALPHABET.includes(val))
+                        .join("");
 
+    for (let i = 0; i < cleaned.length / 2; ++i) {
+        if (cleaned[i] != cleaned[cleaned.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
